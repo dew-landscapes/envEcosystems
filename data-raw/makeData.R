0d26e7e2b707d893735e6d74398fe959b2a258a1
Nigel Willoughby <nige.willoughby@gmail.com>
2021-08-18 08:59:44 +0930

    Added required data.

--- data-raw/makeData.R
+++ data-raw/makeData.R
@@ -1,8 +1,22 @@
 
-  codes <- list.files(path = "data-raw",pattern = "\\.R$", full.names = TRUE)
+  library(magrittr)
+  library(dplyr)
+  library(purrr)
+  library(fs)
+
+  codes <- list.files(path = "data-raw",pattern = "\\.R$", full.names = TRUE) %>%
+    grep("makeData",.,value = TRUE, invert = TRUE)
 
   lapply(codes,source)
 
-  datas <- ls(pattern = "cut|lu|Spp")
+  datas <- ls(pattern = "cut|lu|Spp") %>%
+    tibble::enframe(name = NULL, value = "name") %>%
+    dplyr::mutate(obj = map(name,get)
+                  , savefile = path("data",paste0(name,".rData"))
+                  )
 
-  do.call(save, c(lapply(datas,as.name), file = "R/sysdata.rda"))
+  data <- datas %>%
+    dplyr::pull(obj) %>%
+    stats::setNames(datas$name)
+
+  walk2(data,datas$savefile,~save(.x,file = .y))
