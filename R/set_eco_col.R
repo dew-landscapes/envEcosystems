90bbc3caf04d72a0a264689512bda215cee4663e
Acanthiza <nigel.willoughby@sa.gov.au>
2025-12-10 20:01:49 +1030

    revert set_eco_col manually (and fix git pain)

--- R/set_eco_col.R
+++ R/set_eco_col.R
@@ -21,6 +21,8 @@ set_eco_col <- function(colour, x, y) {
 
   }
 
+  colour <- rlang::ensym(colour)
+
   # x, as in this is x of y
   # y, how many in this colour group?
 
@@ -28,14 +30,33 @@ set_eco_col <- function(colour, x, y) {
   # https://gist.github.com/mbannert/e9fcfa86de3b06068c83
   col2hex <- function(col, alpha) {
 
-    rgb(t(col2rgb(col)), alpha = alpha, maxColorValue = 255)
+    rgb(t(col2rgb(col)), alpha=alpha, maxColorValue=255)
 
   }
 
+<<<<<<< HEAD
   adj <- (y - x + 1) / y
   adj <- scales::rescale(adj, to = c(0.3, 1), from = c(0, 1))
 
   col2hex(rlang::ensym(colour)
           , alpha = 255 * adj
           )
+=======
+  if(y > 1) {
+
+    pal <- colortools::sequential(color = colour
+                                  , percentage = 100*(1/((1+ 1/3)*y))
+                                  , alpha = 1
+                                  , plot = FALSE
+                                  )
+
+    tail(pal,y)[x]
+
+  } else {
+
+    col2hex(colour)
+
+  }
+
+>>>>>>> 33c92fd (revert set_eco_col manually (and fix git pain))
 }
